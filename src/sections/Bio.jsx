3c9739ca61bf75521d47ff275640a97489355212
JagnaJ <janaponomarenk@gmail.com>
2026-02-26 02:34:10 +0100

    fix maitlo problem and added apos for eslint purpose

--- src/sections/Bio.jsx
+++ src/sections/Bio.jsx
@@ -1,32 +1,48 @@
-import { Linkedin, AtSign, GitBranch } from 'lucide-react';
-import myPhoto from "/src/assets/my-photo4.jpg";
-
+import {Linkedin, AtSign, GitBranch} from 'lucide-react';
+import myPhoto from '/src/assets/my-photo4.jpg';
 
 const Bio = () => {
+    const email = 'janaponomarenk@gmail.com';
+
     return (
-        <section className="bg-white text-left rounded-xl p-2 md:p-5 shadow-md">
+        <section className='bg-white text-left rounded-xl p-2 md:p-5 shadow-md'>
             <img
-                src= {myPhoto}
-                alt="my profile photo"
-                className="h-32 w-32 rounded-full"
+                src={myPhoto}
+                alt='my profile photo'
+                className='h-32 w-32 rounded-full'
             />
-            <h1 className="text-lg md:text-2xl font-bold mt-3">Yana Ponomarenko</h1>
-            <p className="text-secondary-foreground py-2">I'm a Frontend Developer</p>
-            <div className="flex flex-row gap-2 mt-2">
-                <a href="https://www.linkedin.com/in/yana-ponomarenko-34775923a/" target="_blank"
-                    rel="noopener noreferrer">
+            <h1 className='text-lg md:text-2xl font-bold mt-3'>
+                Yana Ponomarenko
+            </h1>
+            <p className='text-secondary-foreground py-2'>
+                I&apos;m a Frontend Developer
+            </p>
+            <div className='flex flex-row gap-2 mt-2'>
+                <a
+                    href='https://www.linkedin.com/in/yana-frontend/'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
                     <Linkedin className='text-blue-400 hover:text-blue-500 cursor-pointer' />
                 </a>
-                <a href="mailto:janaponomarenk@gmail.com" target="_blank"
-                    rel="noopener noreferrer">
+                <a
+                    href={`https://mail.google.com/mail/?view=cm&fs=1&to=${email}`}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    aria-label='Send email'
+                >
                     <AtSign className='text-pink-400 hover:text-pink-500 cursor-pointer' />
                 </a>
-                <a href="https://github.com/JagnaJ" target="_blank" rel="noopener noreferrer">
+                <a
+                    href='https://github.com/JagnaJ'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
                     <GitBranch className='text-red-400 hover:text-red-500 cursor-pointer' />
                 </a>
             </div>
         </section>
     );
-}
+};
 
 export default Bio;
